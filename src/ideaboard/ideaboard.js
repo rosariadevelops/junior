ec1c487d6db75d9aa09fcce94e2607f0bffc18d4
Rosaria Gandar <rosariagandar@gmail.com>
2020-10-15 19:02:48 +0200

    Clicking All will render all idea cards again

--- src/ideaboard/ideaboard.js
+++ src/ideaboard/ideaboard.js
@@ -15,11 +15,7 @@ export default function IdeaBoard() {
     const { isVisible, toggle } = useModal();
     const [filter, setFilter] = useState({ filterValue: [] });
     const [renderFilter, setRenderFilter] = useState({ filterOn: false });
-    // const { filterByInput } = useFilterByTag();
     const [create, setCreate] = useState({ createIdea: false });
-    //const [ideas, setIdeas] = useState();
-
-    //const initialIdeas = useSelector((state) => state.ideas);
 
     useEffect(() => {
         dispatch(recieveIdeas());
@@ -28,26 +24,20 @@ export default function IdeaBoard() {
     console.log("ideas: ", ideas);
 
     useEffect(() => {
-        // setRenderFilter({ filterOn: true });
         console.log("renderFilter: ", renderFilter.filterOn);
     }, [filter.filterValue]);
-
-    //useEffect(() => {
-    /* console.log("useEffect filterIdeas: ", filterIdeas);
-    if (filterIdeas && filterIdeas.length > 0) {
-        setRenderFilter({ filterOn: true });
-        console.log("is it true?");
-    } */
-
-    //}, []);
 
     const ideas = useSelector((state) => state.ideas);
 
     function filterByValue(e) {
         const { value } = e.target;
         console.log("value: ", value);
+
         setFilter({ filterValue: value });
         setRenderFilter({ filterOn: true });
+        if (value == "All") {
+            setRenderFilter({ filterOn: false });
+        }
     }
 
     const filterIdeas =
@@ -62,96 +52,6 @@ export default function IdeaBoard() {
         setCreate((create.createIdea = true));
         console.log("create: ", create);
     }
-
-    /* if (renderFilter.filterOn) {
-        return (
-            <React.Fragment>
-                <div className="add-idea">
-                    <div className="plus" onClick={(e) => createIdea(e)}>
-                        <div className="plus-h"></div>
-                        <div className="plus-v"></div>
-                    </div>
-                </div>
-                <div className="title">
-                    <h2>Ideaboard</h2>
-                </div>
-                <div className="ibd-tags-ctr">
-                    <button
-                        onClick={(e) => filterByValue(e)}
-                        className="ibd-tags"
-                        value="React.js"
-                    >
-                        React.js
-                    </button>
-                    <div className="ibd-tags">Vue.js</div>
-                    <div className="ibd-tags">TypeScript</div>
-                    <div className="ibd-tags">Angular</div>
-                    <div className="ibd-tags">JavaScript</div>
-                    <div className="ibd-tags">jQuery</div>
-                    <div className="ibd-tags">Node.js</div>
-                    <div className="ibd-tags">Express</div>
-                </div>
-                <div className="grid-ctr">
-                    {filterIdeas &&
-                        filterIdeas.map((idea) => (
-                            <IdeaCard key={idea.id} idea={idea} />
-                        ))}
-                </div>
-
-                {isVisible && (
-                    <ModalComponent
-                        isVisible={isVisible}
-                        hide={toggle}
-                        create={create}
-                    />
-                )}
-            </React.Fragment>
-        );
-    } else if (!renderFilter.filterOn) {
-        return (
-            <React.Fragment>
-                <div className="add-idea">
-                    <div className="plus" onClick={(e) => createIdea(e)}>
-                        <div className="plus-h"></div>
-                        <div className="plus-v"></div>
-                    </div>
-                </div>
-                <div className="title">
-                    <h2>Ideaboard</h2>
-                </div>
-                <div className="ibd-tags-ctr">
-                    <button
-                        onClick={(e) => filterByValue(e)}
-                        className="ibd-tags"
-                        value="React.js"
-                    >
-                        React.js
-                    </button>
-                    <div className="ibd-tags">Vue.js</div>
-                    <div className="ibd-tags">TypeScript</div>
-                    <div className="ibd-tags">Angular</div>
-                    <div className="ibd-tags">JavaScript</div>
-                    <div className="ibd-tags">jQuery</div>
-                    <div className="ibd-tags">Node.js</div>
-                    <div className="ibd-tags">Express</div>
-                </div>
-                <div className="grid-ctr">
-                    {ideas &&
-                        ideas.map((idea) => (
-                            <IdeaCard key={idea.id} idea={idea} />
-                        ))}
-                </div>
-
-                {isVisible && (
-                    <ModalComponent
-                        isVisible={isVisible}
-                        hide={toggle}
-                        create={create}
-                    />
-                )}
-            </React.Fragment>
-        );
-    } */
 
     return (
         <React.Fragment>
